664791804185f7de0dc41abbd01dbf9062683d54
Jack <chl218@eng.ucsd.edu>
2018-12-01 15:26:14 -0800

    k-mers pattern frequency

--- 00-Finding-Hidden-Messages-in-DNA/01-Where-in-the-Genome-Does-Replication-Begin/patterns.py
+++ 00-Finding-Hidden-Messages-in-DNA/01-Where-in-the-Genome-Does-Replication-Begin/patterns.py
@@ -1,11 +1,10 @@
 # %%
 import numpy as np
 
-# %%
-genome = np.loadtxt('dataset_2_7.txt', dtype='str');
 
 # %%
 
+# Search the amount of pattern matched in text
 def PatternCount(Text, Pattern):
 
     textLen    = len(Text)
@@ -18,6 +17,32 @@ def PatternCount(Text, Pattern):
     
     return count
 
+# Find the most frequent k-mer in text
+def FrequentWords(Text, k):
+    textLen = len(Text)
+    
+    frequentPatterns = set()
+    count = [0] * textLen
+    
+    # Search for k-mer patterns
+    for i in range(0, textLen - k):
+        pattern = Text[i:i+k]
+        count[i] = PatternCount(Text, pattern)
+    maxCount = max(count)
+    
+    # Search for most frequent k-mers
+    for i in range(0, textLen - k):
+        if count[i] == maxCount:
+            frequentPatterns.add(Text[i:i+k])
+            
+    return frequentPatterns
 
 # %%    
+genome = np.loadtxt('dataset_2_7.txt', dtype='str');
 print(PatternCount(genome[0], genome[1]))
+
+# %%
+genome = np.loadtxt('dataset_2_10.txt', dtype='str');
+res = FrequentWords(genome[0], int(genome[1]))
+for seq in res:
+    print(seq, ' ', end='')
