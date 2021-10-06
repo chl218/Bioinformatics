7a4778b006f7dc14368163da7a33f8c804f3522f
Li, Jack <chl218@eng.ucsd.edu>
2021-10-05 22:27:13 -0700

    freq words fnc, docs, test clean up

---
+++ 01_Finding_Hidden_Messages_In_DNA/src/dna_pattern_finding.py
@@ -0,0 +1,84 @@
+
+from typing import Dict, List, Set
+
+
+class DNAPatternFinding:
+
+    """ Finding Hidden Messages in DNA
+    """
+
+    def __init__(self) -> None:
+        pass
+
+
+    def pattern_count(self, text: str, pattern: str) -> int:
+        """ Pattern Count
+
+        The number of times that a k-mer pattern appears as a substring of text.
+        """
+        count = 0
+        pattern_len = len(pattern)
+        for i in range(0, len(text) - len(pattern) + 1):
+            j = i + pattern_len
+            if text[i:j] == pattern:
+                count += 1
+
+        return count
+
+
+    def frequent_words(self, text: str, k: int) -> List[str]:
+        """ Frequent Words
+
+        Finding the most frequent k-mers in a string text
+        """
+
+        frequent_patterns = set()
+        counts = []
+        n = len(text) - k + 1
+
+        for i in range(0, n):
+            counts.append(self.pattern_count(text, text[i:i+k]))
+
+        max_count = max(counts)
+        for i in range(0, n):
+            if counts[i] == max_count:
+                frequent_patterns.add(text[i:i+k])
+
+        return list(frequent_patterns)
+
+
+    def frequency_table(self, text: str, k: int) -> Dict[str, int]:
+        """ Frequency Table
+
+        Takes a string text and an integer k as input and returns their frequency
+        table as a map of string keys to integer values.
+        """
+
+        freq_map = {}
+        n = len(text)
+
+        for i in range(0, n-k+1):
+            pattern = text[i:i+k]
+            if pattern in freq_map:
+                freq_map[pattern] += 1
+            else:
+                freq_map[pattern] = 1
+
+        return freq_map
+
+
+    def better_frequent_words(self, text: str, k: int) -> List[str]:
+        """ Frequent Words (Optimized)
+
+        Finding the most frequent k-mers in a string text
+        """
+
+        frequent_patterns = []
+        frequent_map = self.frequency_table(text, k)
+
+        max_frequency = max(frequent_map.values())
+        for k, v in frequent_map.items():
+            if frequent_map[k] == max_frequency:
+                frequent_patterns.append(k)
+
+        return frequent_patterns
---
+++ 01_Finding_Hidden_Messages_In_DNA/test/test_dna_pattern_finding.py
@@ -0,0 +1,62 @@
+import unittest
+import numpy as np
+from src.dna_pattern_finding import DNAPatternFinding
+
+
+class TestDNAPatternFinding(unittest.TestCase):
+
+    def __init__(self, methodName: str = ...) -> None:
+        super().__init__(methodName=methodName)
+
+        self.uut = DNAPatternFinding()
+        self.dataPath = "01_Finding_Hidden_Messages_In_DNA/data"
+
+
+    def read_data(self, file_path: str, read_type: str, file_count: int) -> list:
+        data = []
+        for i in range(1, file_count):
+            res = np.loadtxt(file_path+str(i)+".txt", dtype=read_type)
+            if res.size == 1:
+                data.append([res.tolist()])
+            else:
+                data.append(res.tolist())
+        return data
+
+
+    def test_pattern_count(self):
+
+        inputs = self.read_data(self.dataPath+"/pattern_count_inputs/input_", "str", 9)
+        expected = self.read_data(self.dataPath+"/pattern_count_outputs/output_", "int", 9)
+        actual = []
+        for input in inputs:
+            actual.append([self.uut.pattern_count(input[0], input[1])])
+
+        self.assertEqual(expected, actual)
+
+
+    def test_frequent_words(self):
+
+        inputs = self.read_data(self.dataPath+"/frequent_words_inputs/input_", "str", 7)
+        expected = self.read_data(self.dataPath+"/frequent_words_outputs/output_", "str", 7)
+        actual = []
+        for input in inputs:
+            actual.append(self.uut.frequent_words(input[0], int(input[1])))
+
+        for a1, a2 in zip(expected, actual):
+            self.assertCountEqual(a1, a2)
+
+
+    def test_better_frequent_words(self):
+
+        inputs = self.read_data(self.dataPath+"/frequent_words_inputs/input_", "str", 7)
+        expected = self.read_data(self.dataPath+"/frequent_words_outputs/output_", "str", 7)
+        actual = []
+        for input in inputs:
+            actual.append(self.uut.better_frequent_words(input[0], int(input[1])))
+
+        for a1, a2 in zip(expected, actual):
+            self.assertCountEqual(a1, a2)
+
+
+if __name__ == '__main__':
+    unittest.main()
