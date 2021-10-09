89be16fbd6c1f386a2f6a962135a59d7de9fe1f7
Li, Jack <chl218@eng.ucsd.edu>
2021-10-09 12:57:20 -0700

    GC skew

--- 01_Finding_Hidden_Messages_In_DNA/src/genome_replication_algorithm.py
+++ 01_Finding_Hidden_Messages_In_DNA/src/genome_replication_algorithm.py
@@ -1,2 +1,33 @@
+from typing import List
+
+
 class GenomeReplicationAlgorithm:
-    pass
+
+    def __init__(self) -> None:
+        pass
+
+    def skew_GC(self, genome: str) -> List[int]:
+
+        diff = 0
+        skew = [0]
+
+        n = len(genome)
+        for i in range(0, n):
+            if genome[i] == 'G':
+                diff += 1
+                skew.append(diff)
+            elif genome[i] == 'C':
+                diff -= 1
+                skew.append(diff)
+            else:
+                skew.append(diff)
+
+        return skew
+
+
+
+
+uut = GenomeReplicationAlgorithm()
+g = "GAGCCACCGCGATA"
+
+print(' '.join(map(str, uut.skew_GC(g))))
