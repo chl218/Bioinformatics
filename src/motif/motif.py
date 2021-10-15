c7ba3bbec0b32aba340b4fbb3b34544b81a073bb
Li, Jack <chl218@eng.ucsd.edu>
2021-10-14 22:17:26 -0700

    comments

--- src/motif/motif.py
+++ src/motif/motif.py
@@ -14,23 +14,29 @@ class MotifAlgorithm:
         self.gra = GenomeReplicationAlgorithm()
 
     def motif_enumeration(self, dna_list: List[str], k: int, d: int) -> List[str]:
-        motifs = set()
+        """ Motif Enumeration
 
+        Given a collection of strings DNA and an integer d, a k-mer is a
+        (k,d)-motif if it appears in every string from Dna with at most d
+        mismatches. Find all (k, d)-motifs in DNA.
+        """
+
+        motifs = set()
         dna = ''.join(dna_list)
         n = len(dna) - k + 1
 
+        # for each k-mer Pattern in the first string in Dna
         for i in range(0, n):
             pattern = dna[i:i+k]
             neighbors = self.gra.neighbors(pattern, d)
 
+            # for each k-mer Pattern’ differing from Pattern by at most d mismatches
             for pattern_d in neighbors:
+                # if Pattern' appears in each string from Dna with at most d mismatches
                 containsd = [self.gra.approximate_pattern_count(pattern_d, dna, d) for dna in dna_list]
-
                 if all([count > 0 for count in containsd]):
                     motifs.add(pattern_d)
 
         return list(motifs)
 
 
-
-print(' '.join(uut.motif_enumeration(g, k, d)))
